8036c68b87ce2d7317da8fe30ac26b7be6cbd5c5
Ian Lockett <mail@ianlockett.co.uk>
2018-09-25 20:42:18 +0100

    script fix (again)

--- update_podspec.rb
+++ update_podspec.rb
@@ -1,6 +1,6 @@
 replacement_string = ARGV[0]
 
-text = File.read(Demo.podspecTemplate)
+text = File.read("Demo.podspecTemplate")
 new_contents = text.gsub(/{podspec_version}/, replacement_string)
 
 # To write changes to the file, use:
